4c1b4a49d9d0978f6f8848b780120e9b0b74cd2a
michelineuh <gaelschenck@hotmail.fr>
2024-02-24 14:52:27 +0100

    24/02

--- assets/app.js
+++ assets/app.js
@@ -6,5 +6,6 @@
  * which should already be in your base.html.twig.
  */
 import './styles/app.scss';
+import 'tw-elements'
 
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -8,10 +8,34 @@ use Symfony\Component\Routing\Attribute\Route;
 
 class DefaultController extends AbstractController
 {
-    #[Route('/default', name: 'app_default')]
+    #[Route('/home', name: 'home')]
     public function index(): Response
     {
-        return $this->render('default/index.html.twig', [
+        return $this->render('default/home.html.twig', [
+            'controller_name' => 'DefaultController',
+        ]);
+    }
+
+    #[Route('/patisserie', name: 'app_patisserie')]
+    public function patisserie(): Response
+    {
+        return $this->render('default/patisserie.html.twig', [
+            'controller_name' => 'DefaultController',
+        ]);
+    }
+
+    #[Route('/boulangerie', name: 'app_boulangerie')]
+    public function boulangerie(): Response
+    {
+        return $this->render('default/boulangerie.html.twig', [
+            'controller_name' => 'DefaultController',
+        ]);
+    }
+
+    #[Route('/contact', name: 'app_contact')]
+    public function contact(): Response
+    {
+        return $this->render('default/contact.html.twig', [
             'controller_name' => 'DefaultController',
         ]);
     }
